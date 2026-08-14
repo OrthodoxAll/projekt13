9b856bab12f2c2158abdd361a815ac155b593b1d
OrthodoxAll <aleksandrnagiev@yandex.ru>
2026-08-14 22:54:49 +0700

    add funcs

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def foo()
-    pass
+def add_numbers(num1, num2):
+    return num1 + num2
 
 
-def bar();
-    pass
+
+def is_even(num):
+    return num % 2 == 0
+
+
+
+def find_max(my_list):
+    if len(my_list) > 0:
+         return max(my_list)
+    return 0
+
+
+
+if __name__ == '__main__':
+    assert add_numbers(1, 2) == 3
+
+    assert is_even(0) == True
+
+    assert find_max([]) == 5
